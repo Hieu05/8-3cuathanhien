862272b2dbd57043adc7035f42ea07762387ff3b
bé Hiếu <hieudang.05052004@gmail.com>
2023-03-06 12:47:49 +0700

    upload code moi

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng Cô Gái Của Anh';
+const contentCard = 'Chúc Bé iu của anh 08/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Hiếu https://www.facebook.com/HieuFF.05/
+const giftImage = 'beiu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
